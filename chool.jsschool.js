f3f473214c1cbe224969c3f42311ef3e201cf8d8
Denis <tetragrom@mail.ru>
2020-10-30 20:13:14 +0400

    Убрал строки VS

---
+++ chool.jsschool.js
@@ -0,0 +1,61 @@
+warning: LF will be replaced by CRLF in .idea/workspace.xml.
+The file will have its original line endings in your working directory
+[1mdiff --git a/.idea/workspace.xml b/.idea/workspace.xml[m
+[1mindex ad0e01e..ba499f2 100644[m
+[1m--- a/.idea/workspace.xml[m
+[1m+++ b/.idea/workspace.xml[m
+[36m@@ -2,6 +2,8 @@[m
+ <project version="4">[m
+   <component name="ChangeListManager">[m
+     <list default="true" id="4d38a486-d18d-432b-831e-7af656dc9a2a" name="Default Changelist" comment="">[m
+[32m+[m[32m      <change afterPath="$PROJECT_DIR$/.idea/vcs.xml" afterDir="false" />[m
+[32m+[m[32m      <change beforePath="$PROJECT_DIR$/.idea/workspace.xml" beforeDir="false" afterPath="$PROJECT_DIR$/.idea/workspace.xml" afterDir="false" />[m
+       <change beforePath="$PROJECT_DIR$/school.js" beforeDir="false" afterPath="$PROJECT_DIR$/school.js" afterDir="false" />[m
+     </list>[m
+     <ignored path="$PROJECT_DIR$/.tmp/" />[m
+[36m@@ -22,8 +24,8 @@[m
+       <file pinned="false" current-in-tab="true">[m
+         <entry file="file://$PROJECT_DIR$/school.js">[m
+           <provider selected="true" editor-type-id="text-editor">[m
+[31m-            <state relative-caret-position="150">[m
+[31m-              <caret line="10" column="51" selection-start-line="10" selection-start-column="51" selection-end-line="10" selection-end-column="51" />[m
+[32m+[m[32m            <state relative-caret-position="210">[m
+[32m+[m[32m              <caret line="14" lean-forward="true" selection-start-line="14" selection-end-line="14" />[m
+             </state>[m
+           </provider>[m
+         </entry>[m
+[36m@@ -56,7 +58,7 @@[m
+     <other-services-enabled>true</other-services-enabled>[m
+     <auto-save>true</auto-save>[m
+   </component>[m
+[31m-  <component name="ProjectFrameBounds" extendedState="6">[m
+[32m+[m[32m  <component name="ProjectFrameBounds" extendedState="7">[m
+     <option name="x" value="260" />[m
+     <option name="y" value="80" />[m
+     <option name="width" value="1400" />[m
+[36m@@ -160,12 +162,12 @@[m
+       <workItem from="1549604310859" duration="1618000" />[m
+       <workItem from="1549606347465" duration="1242000" />[m
+       <workItem from="1549783556431" duration="10000" />[m
+[31m-      <workItem from="1604071541448" duration="755000" />[m
+[32m+[m[32m      <workItem from="1604071541448" duration="1044000" />[m
+     </task>[m
+     <servers />[m
+   </component>[m
+   <component name="TimeTrackingManager">[m
+[31m-    <option name="totallyTimeSpent" value="13109000" />[m
+[32m+[m[32m    <option name="totallyTimeSpent" value="13398000" />[m
+   </component>[m
+   <component name="TodoView">[m
+     <todo-panel id="selected-file">[m
+[36m@@ -267,8 +269,8 @@[m
+     </entry>[m
+     <entry file="file://$PROJECT_DIR$/school.js">[m
+       <provider selected="true" editor-type-id="text-editor">[m
+[31m-        <state relative-caret-position="150">[m
+[31m-          <caret line="10" column="51" selection-start-line="10" selection-start-column="51" selection-end-line="10" selection-end-column="51" />[m
+[32m+[m[32m        <state relative-caret-position="210">[m
+[32m+[m[32m          <caret line="14" lean-forward="true" selection-start-line="14" selection-end-line="14" />[m
+         </state>[m
+       </provider>[m
+     </entry>[m
